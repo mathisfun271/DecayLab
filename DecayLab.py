c5f71d4458703d94033ee40eac6e2103ba018c40
mathisfun271 <64607637+mathisfun271@users.noreply.github.com>
2020-05-15 22:46:46 -0500

    Update DecayLab.py

--- DecayLab.py
+++ DecayLab.py
@@ -24,14 +24,16 @@ while cur != 0:
 
     cur = new
     
-    print('Remaining: %s, Stage time: %.3f' % (cur,time()-st))
+    print('Remaining: %s, Stage time: %.3f' % (cur,time()-st))#Prints status update
     
     count += 1
     xaxis.append(10*count)
-    yaxis.append(cur)
+    yaxis.append(cur)#Appends list for use in graph
 
-print(time()-tst)
+print(time()-tst)#Prints time elapsed over whole code
 
+
+#Graph Rendering in matplotlib
 
 x = np.linspace(0, 10*count, 500)
 y = start*np.exp(-log(2)*x/10)
@@ -44,8 +46,8 @@ ax.scatter(xaxis,yaxis)
 ax.set_xlabel('Seconds')
 ax.set_ylabel('Amount of Particles')
 plt.title('Computer Generated Decay of Candium (Linear)')
-fig.savefig('/Users/nathanmihm/Math Graphics/Decay1.png',dpi=200)
+fig.savefig('/Users/nathanmihm/Math Graphics/Decay1.png',dpi=200)#Linear Save
 
 plt.title('Computer Generated Decay of Candium (Logarithmic)')
 ax.set_yscale('log')
-fig.savefig('/Users/nathanmihm/Math Graphics/Decay2.png',dpi=200)
+fig.savefig('/Users/nathanmihm/Math Graphics/Decay2.png',dpi=200)#Logarithmic Save
